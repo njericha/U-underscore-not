df74a85aed2ae4dda021df84077f515ee3a0ba29
njericha <njericha@edu.uwaterloo.ca>
2017-03-09 16:16:07 -0500

    add toggles

---
+++ Side Project Theory!.py
@@ -0,0 +1,39 @@
+## Theory/Postulates:
+# U_0 is a universe with 2 spatial Dimensions and one temporal Dimension. 
+# The net energy is 0! Local positives and negatives can and will occur! 
+# The initial state (U @ t = 0) is a random distribution of positive and 
+# negative energy! 
+
+
+# Every generation each cell distributes a portion of its current energy via 
+# an energy distribution function E(t).
+
+# Every generation some portion of a cells energy is converted to temperature 
+# via the energy conversion function C(t). 
+
+
+# Rule 42: 
+# if a cell has energy value = to thresh hold value then it does not absorbe 
+# nor distribute energy. (we can use this to make closed shapes, and tunnels)
+
+# Let there exist two types of objects: 
+# these objects exist as additional functions which only appear on certain cells
+# these functions are distributed by the user when setting the initial state
+#    "Absorber": This cell has a function A(t) which randomly chooses an 
+#       adjacent cell and reduces its energy value, that energy is transfered
+#       and classified in the original cell under Stored energy. This cell has
+#       a finite lifetime i.e, the function will be deleted after n generations
+#    "Transporter": This cell has a function B(t) which selects an adjacent cell 
+#       and in the next generation the function is shifted to that cell. It has 
+#       a finite number of "free steps". If the transporter encounters an 
+#       absorber it will "latch" onto it. Then both cells will be transported
+#       togethers. It will steal energy from the stored energy of the absorber
+#       for every time they are transported together. The transporter will "die"
+#       if it runs out of "free steps" and does not have an absorber to steal
+#       from.
+
+# ^^ Seems complicated but it provides a way for the simulation to be more
+# dynamic and allow for energy to be moved in a different form other then 
+# simply distributed. Also I am gonna start coming up with these mathematical 
+# functions. Do you guys have any idea on how we can incorporate reproduction
+# and random sub cell mutation [i.e the baby cells have a different A(t), B(t)]
--- Side-Project.py
+++ Side-Project.py
@@ -6,32 +6,44 @@
 
 import random 
 
+##----rules toggle go here----##
+
+rule_energy = True
+rule_threshold = True
+
 ##----constents go in here----##
 
 t3x3=[[0,0,0],\
       [0,0,0],\
       [0,0,0]]
 
-defult_table=[[0, 0, 0, 0, 0, 0],\
-              [0, 8.0, 0, 0, 0, 0],\
-              [0, 0, 336, 0, 0, 0],\
-              [0, 0, 0, 0, 0, 0],\
-              [0, 0, 0, 0, 0, 0],\
-              [0, 0, 0, 0, 0, 0]]
+defult_table=[[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],\
+              [0.0, 8.0, 0.0, 0.0, 0.0, 0.0],\
+              [0.0, 0.0, 336, 0.0, 0.0, 0.0],\
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],\
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],\
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
               
 dtrt = 2 #<<-- number of decimals to round the printed values
 
 threshold = 42 #<<-- must be an int
 
+
 ##------rules go in here------##
 
 #example of a rule that lowers a value if it is greater than 1
-def rule1(table,row,column,value):
-    if value > 0:
-        table[row][column] = value - 1
+##def rule1(table,row,column,value):
+    ##if value > 0:
+        ##table[row][column] = value - 1
 
-#radiates an 8th of a cells energy to adjacent cells (unless it equals threshold) 
-def energy_rule(new,row,column,old):
+def energy_rule(new,row,column,old,value):
+    """
+    radiates an 8th of a cells energy to adjacent cells
+    (unless it equals threshold) 
+    
+    """
+    if not rule_energy: #<- checks if this rule is turned on
+        return value
     new_value=0
     len_row = len(old)
     len_column = len(old[0])
@@ -48,33 +60,40 @@ def energy_rule(new,row,column,old):
     new[row][column] = new_value / 8
     return new_value / 8
 
-#if a cell has an integer of threshold, it will not radiate energy and ajacent 
-# cells will get an 8th of it's energy back (i.e. energy gets bounced back)
 def rule42(value):
-    if round(value) == threshold:
+    """
+    if a cell has an integer of threshold, it will not radiate energy and 
+    ajacent cells will get an 8th of it's energy back
+    (i.e. energy gets bounced back)
+    """   
+    if rule_threshold and round(value) == threshold:
         return True
     else:
         return False
-        
 
 
 ##--helper functions go here---##
 
-#takes a table and itterates the next generation
+
 def next_generation(table):
+    """
+    takes a table and itterates the next generation
+    """
     next_table = table_copy(table)
     for i in range(0,len(table)):
         for j in range(0,len(table[0])): 
             #rules go in here ## <<-----------           
             value = table[i][j]
             if not rule42(value):               
-                new_value = energy_rule(next_table,i,j,table)
+                new_value = energy_rule(next_table,i,j,table,value)
             else:
                 pass
     return next_table
 
-#takes x and y and outputs a blank table with x rows and y columns
 def generate_table(x,y):
+    """
+    takes x and y and outputs a blank table with x rows and y columns
+    """    
     table = []
     row = []
     while y>0: 
@@ -85,23 +104,60 @@ def generate_table(x,y):
         x += -1
     return table
 
-#takes a table and prints it formated to the screen
 def print_table(table):
+    """
+    takes a table and prints it formated to the screen
+    """
     for row in table:
         print(list(map(lambda x: round(x,dtrt),row)))
     return None
 
-#use this if you don't want to use table[i][j]
 def change_table(table,row,column,value):
-    table[row][column]=value
+    """
+    use this if you don't want to use table[i][j]
+    """
+    table[row-1][column-1]=value
     return
 
-#outputs a coppied version of table. DON'T USE table.copy() TO COPY A TABLE!!
 def table_copy(table):
+    """
+    Outputs a coppied version of table.
+    DON'T USE "table.copy()" TO COPY A TABLE!!
+    """
     copy = []
     for row in table:
         copy.append(row.copy())
     return copy
+
+def table_builder():
+    """
+    Asks user to make a table of size (row by column) and input spicific values 
+    for the table. Returns the table.
+    """
+    table=[]
+    #asks for size of table
+    while True: 
+        row = input("Enter how many rows: ")
+        column = input("Enter how many columns: ")
+        table=generate_table(int(row),int(column))
+        print_table(table)
+        okay = input("This okay? ('y' or 'n'): ")
+        if okay == 'y':
+            break           
+    #asks user for initial values
+    while True: 
+        okay = input("Would you like to change an initial state? ('y' or 'n'): ")
+        if okay == 'n':
+            break            
+        row = int(input("Enter a row to edit: ")) - 1
+        column = int(input("Enter a column to edit: ")) - 1
+        value = int(input("Enter a value: "))
+        tablecopy = table_copy(table)
+        change_table(tablecopy,row,column,value)
+        table = tablecopy
+        print_table(table)
+    return table
+
 
 #the main function
 def U0():
@@ -109,30 +165,8 @@ def U0():
     use_defult= input("Use defult table? ('y' or 'n'): ")
     
     if use_defult == 'n':
-        table=[]
-        # asks user to make the table of size rowXcolumn if defult is not wanted
-        while True: 
-            row = input("Enter how many rows: ")
-            column = input("Enter how many columns: ")
-            table=generate_table(int(row),int(column))
-            print_table(table)
-            okay = input("This okay? ('y' or 'n'): ")
-            if okay == 'y':
-                break           
-        #asks user for initial values
-        while True: 
-            okay = input("Would you like to change an initial state? ('y' or 'n'): ")
-            if okay == 'n':
-                break            
-            row = int(input("Enter a row to edit: ")) - 1
-            column = int(input("Enter a column to edit: ")) - 1
-            value = int(input("Enter a value: "))
-            tablecopy = table_copy(table)
-            change_table(tablecopy,row,column,value)
-            table = tablecopy
-            print_table(table)
-
-    
+        table = table_builder()
+ 
     g = int(input("How many generations would you like?: "))
     n = 0
     while n <= g: #prints all the generations asks
@@ -143,5 +177,7 @@ def U0():
         n += 1
     input("End")
     return None
+
+
 
 U0()
